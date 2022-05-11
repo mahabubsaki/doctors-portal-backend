bad31607b741922f8b161b7d90836165b6752110
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-11 22:33:32 +0600

    successfully connected db to node

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
@@ -11,3 +12,21 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Doctors Portal running on port ${port}`)
 })
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wcxgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctorsPortal').collection('services')
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
